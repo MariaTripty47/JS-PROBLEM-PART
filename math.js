90a97b75613cd64b24582eb1625162d094e725f9
tishalaa <marianurtripty47@gmail.com>
2024-08-20 21:08:56 +0600

    included a new file

---
+++ math.js
@@ -0,0 +1,33 @@
+
+// for find min
+const min = Math.min(45,1,65,99,126);
+console.log(min);
+
+// for find max
+const max = Math.max(45,1,65,99,126);
+console.log(max);
+
+console.log(Math.PI)
+// Returns the absolute value of a number 
+// (the value without regard to whether it is positive or negative)
+console.log(Math.abs(5-10))
+
+// The value to be rounded to the nearest integer.
+// Returns a supplied numeric expression rounded to the nearest integer.
+console.log(Math.round(2.25))
+
+// A numeric expression.
+// Returns the greatest integer less than or equal to its numeric argument.
+console.log(Math.floor(3.75))
+
+// Returns the smallest integer greater than or equal to its numeric argument.
+console.log(Math.ceil(3.75))
+
+// Returns a pseudorandom number between 0 and 1.
+console.log(Math.random()*10)
+const rand = Math.round(Math.random()*10)
+console.log(rand)
+
+
+
+
